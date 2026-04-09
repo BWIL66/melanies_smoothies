3a7c5c388b0ef4f87900f998c67d04aa115a63c7
BWIL66 <fansportauto99@gmail.com>
2026-04-09 15:23:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,5 +56,7 @@ if ingredients_list :
     #st.write(my_insert_stmt)    
     #if ingredients_string:
     #    session.sql(my_insert_stmt).collect()
-        
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
