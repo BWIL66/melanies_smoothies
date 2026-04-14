54847dd188ffab2452a115346462131521da6720
BWIL66 <fansportauto99@gmail.com>
2026-04-14 14:45:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df=my_dataframe.topandas()
 #st.dataframe(data=my_dataframe, width='stretch')
 #st.stop
 
@@ -28,7 +29,6 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-
 if ingredients_list :
     ingredients_string='';
 
@@ -36,7 +36,6 @@ if ingredients_list :
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        pd_df.toPandas()
        
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
